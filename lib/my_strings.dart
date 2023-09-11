aa036f3a78ca42a2cd8c18729f31aae43dfa47df
mrowshanzamir <mrowshanzamir@gmail.com>
2023-09-11 17:14:55 +0330

    add FlutterGen package

---
+++ lib/my_strings.dart
@@ -0,0 +1,14 @@
+class MyProjectStrings{
+  static const viewHotestPosts = "مشاهده داغ ترین نوشته ها";
+  static const viewHotestPodcasts = "مشاهده داغ ترین پادکست ها";
+  static const game = "بازی رایانه ای";
+  static const cryptocurrencies = "رمز ارزها";
+  static const programing = "برنامه نویسی";
+  static const articleLists = "لیست مقاله ها";
+  static const podcastLists = "لیست پادکست ها";
+  static const relatedArticles = "نوشته های مرتبط";
+  static const submitArticle = "ارسال مقاله";
+  static const submitPodcast = "ارسال پادکست";
+  // static const game = "";
+  // static const game = "";
+}
